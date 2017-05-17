5b84888caa40d521a39215a4194275b7b0370da4
sowaira <sara.slila7@gmail.com>
2017-05-17 19:52:11 +0100

    chnage email url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.action_mailer.default_url_options = { host:"http://localhost:3000" }
+  config.action_mailer.default_url_options = { host:"http://appoint-me.herokuapp.com/" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
